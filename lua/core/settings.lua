97c97f9b2546b21c1a4c4936b2dc8abe3479cc6f
Constantine Bulany <robpirojk@gmail.com>
2021-10-16 19:28:54 +0300

    Disabled F1

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -21,6 +21,8 @@ map('i', 'ii', '<esc>', {noremap = true})           -- Better ESC mapping
 map('t', 'ii', '<c-\\><c-n>', {noremap = true})     -- Escape terminal the same way
 map('t', '<esc>', '<c-\\><c-n>', {noremap = true})
 map('', '<space>', '<leader>', {noremap = false})   -- Better Leader mapping
+map('n', '<f1>', '<nop>', {noremap = false})         -- Disable F1
+map('i', '<f1>', '<nop>', {noremap = false})         -- Disable F1
 
 local disabled_built_ins = {
     'netrw',
