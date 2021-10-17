eb988587457c1c3cdaf21bac6b09d8cc025f22de
Constantine Bulany <robpirojk@gmail.com>
2021-10-17 21:58:15 +0300

    Added rust-analyzer

---
+++ lua/lang/server/rust.lua
@@ -0,0 +1,32 @@
+-- Rust analyzer
+local capabilities = require('lang.completion.capabilities')
+local util = require('lspconfig.util')
+require('lspconfig').rust_analyzer.setup{
+    cmd = { 'rust-analyzer' },
+    filetypes = { 'rust' },
+    root_dir = function (fname)
+        local root_files = {
+            'Cargo.toml',
+            'rust-project.json'
+        }
+        return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+    end,
+    settings = {
+      ['rust-analyzer'] = {
+            assist = {
+                importGranularity = 'module',
+                importPrefix = 'by_self',
+            },
+            cargo = {
+                loadOutDirsFromCheck = true
+            },
+            procMacro = {
+                enable = true
+            },
+      },
+    },
+    flags = {
+        debounce_text_changes = 150,
+    },
+    capabilities = capabilities,
+}
