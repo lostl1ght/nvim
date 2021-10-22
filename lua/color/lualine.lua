44adbe4b42c12d07a6f391a2eea0f26f5ed306c0
Constantine Bulany <robpirojk@gmail.com>
2021-10-22 21:30:03 +0300

    Added man to lualine ignored filtypes

--- lua/color/lualine.lua
+++ lua/color/lualine.lua
@@ -4,7 +4,7 @@ require('lualine').setup({
     options = {
         theme = 'material-nvim',
         icons_enabled = true,
-        disabled_filetypes = {'alpha', 'NvimTree',},
+        disabled_filetypes = {'alpha', 'NvimTree', 'man'},
     },
     sections = {
         lualine_b = {
--- lua/color/material.lua
+++ lua/color/material.lua
@@ -2,7 +2,7 @@
 vim.o.termguicolors = true
 vim.o.cursorline = true
 --vim.g.material_style = 'lighter'
-vim.g.material_style = 'darker'
---vim.g.material_style = 'oceanic'
+-- vim.g.material_style = 'darker'
+vim.g.material_style = 'oceanic'
 vim.cmd('colorscheme material')
 
