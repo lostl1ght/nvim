70b46c4a0b68905c2611587dc9e9b724867709c5
Constantine Bulany <robpirojk@gmail.com>
2021-10-24 01:13:39 +0300

    Added feline

---
+++ lua/color/feline/material-darker.lua
@@ -0,0 +1,15 @@
+local colors = {
+    bg = '#212121',
+    fg = '#abb2bf',
+    yellow = '#e0af68',
+    cyan = '#56b6c2',
+    darkblue = '#081633',
+    green = '#98c379',
+    orange = '#d19a66',
+    violet = '#a9a1e1',
+    magenta = '#c678dd',
+    blue = '#61afef',
+    red = '#e86671'
+}
+
+return colors
