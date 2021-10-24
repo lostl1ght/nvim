f33637651dbcb8beb37a526a984fbca39af4cbd0
Constantine Bulany <robpirojk@gmail.com>
2021-10-24 09:47:26 +0300

    Removed autocmd

--- lua/ll/plugin/loader.lua
+++ lua/ll/plugin/loader.lua
@@ -3,46 +3,40 @@ local M = {}
 function M:init()
     local cmd = vim.cmd
 
-    local present, packer = pcall(require, "packer")
+    local present, packer = pcall(require, 'packer')
 
     if not present then
-        local packer_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+        local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
-        print "Cloning packer.."
+        print 'Cloning packer..'
         -- remove the dir before cloning
-        vim.fn.delete(packer_path, "rf")
+        vim.fn.delete(packer_path, 'rf')
         vim.fn.system {
-            "git",
-            "clone",
-            "https://github.com/wbthomason/packer.nvim",
-            "--depth",
-            "20",
+            'git',
+            'clone',
+            'https://github.com/wbthomason/packer.nvim',
+            '--depth',
+            '20',
             packer_path,
         }
 
-        cmd "packadd packer.nvim"
-        present, packer = pcall(require, "packer")
+        cmd 'packadd packer.nvim'
+        present, packer = pcall(require, 'packer')
 
         if present then
-            print "Packer cloned successfully."
+            print 'Packer cloned successfully.'
         else
-            error("Couldn't clone packer !\nPacker path: " .. packer_path .. "\n" .. packer)
+            error('Couldn\'t clone packer !\nPacker path: ' .. packer_path .. '\n' .. packer)
+            return
         end
     end
 
-    cmd([[
-        augroup packer_user_config
-            autocmd!
-            autocmd BufWritePost list.lua source <afile> | PackerCompile
-        augroup end
-    ]])
-
-    cmd "packadd packer.nvim"
+    cmd 'packadd packer.nvim'
 
     packer.init {
     display = {
         open_fn = function()
-            return require("packer.util").float { border = 'rounded' }
+            return require('packer.util').float { border = 'rounded' }
         end,
     },
     git = {clone_timeout = 600},
