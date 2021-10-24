a373395f499a62f7f87ef33b005248daf722ab5a
Constantine Bulany <robpirojk@gmail.com>
2021-10-24 19:11:57 +0300

    Added material config

--- lua/ll/color/material.lua
+++ lua/ll/color/material.lua
@@ -2,9 +2,44 @@ local present, material = pcall(require, 'material')
 if not present then
     return print('material not found')
 end
--- Color scheme
+
 vim.o.termguicolors = true
 vim.o.cursorline = true
 vim.g.material_style = vim.g.color_theme
+
+material.setup({
+	contrast = true,
+	borders = true,
+
+	popup_menu = 'colorful', --'dark', 'light', 'colorful' or 'stealth'
+
+	italics = {
+		comments = true,
+		keywords = false,
+		functions = false,
+		strings = true,
+		variables = false
+	},
+
+	contrast_windows = {
+		'terminal',
+		'packer',
+		'qf'
+	},
+
+	text_contrast = {
+		lighter = false,
+		darker = false
+	},
+
+	disable = {
+		background = false,
+		term_colors = false,
+		eob_lines = true
+	},
+
+	custom_highlights = {}
+})
+
 vim.cmd('colorscheme material')
 
