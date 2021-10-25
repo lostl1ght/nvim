cbe2432df0adea4877edb505c0002ede2e6affb8
Constantine Bulany <robpirojk@gmail.com>
2021-10-25 04:18:11 +0300

    Added treesitter folding

--- lua/ll/lang/treesitter.lua
+++ lua/ll/lang/treesitter.lua
@@ -12,9 +12,13 @@ treesitter_configs.setup {
         'latex',
         'lua',
         'python',
+        'rust',
     },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     }
 }
+vim.api.nvim_command('set foldmethod=expr')
+vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+vim.api.nvim_command('set nofoldenable')
--- lua/ll/plugin/config/telescope.lua
+++ lua/ll/plugin/config/telescope.lua
@@ -2,17 +2,30 @@ local present, tele = pcall(require, 'telescope')
 if not present then
     return print('telescope not found')
 end
+
+local telescope_actions = require("telescope.actions.set")
+
+local attach_mappings = function(_)
+    telescope_actions.select:enhance({
+        post = function()	vim.cmd(":normal! zx zi") end,
+    })
+    return true
+end
+
 -- Telescope setup
 tele.setup{
     pickers = {
         find_files = {
             hidden = true,
+            attach_mappings = attach_mappings,
         },
         live_grep = {
             hidden = true,
+            attach_mappings = attach_mappings,
         },
         help_tags = {
             hidden = true,
+            attach_mappings = attach_mappings,
             mappings = {
                 i = {
                     ['<cr>'] = 'select_tab'
@@ -23,18 +36,12 @@ tele.setup{
             },
         },
         file_browser = {
+            attach_mappings = attach_mappings,
             hidden = true,
         },
         oldfiles = {
+            attach_mappings = attach_mappings,
             hidden = true,
         },
     },
-    extensions = {
-        project = {
-            base_dirs = {
-                '~/code'
-            },
-            hidden_files = true
-        }
-    }
 }
