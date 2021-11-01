3cb36054fe4ef8b81111cef1d46da815eab32670
Constantine Bulany <robpirojk@gmail.com>
2021-11-01 09:17:45 +0300

    New clear bufs

--- lua/ll/util/init.lua
+++ lua/ll/util/init.lua
@@ -10,5 +10,26 @@ function M.close_term()
     end
 end
 
+function M.is_normal_buffer(buffer)
+  return #vim.api.nvim_buf_get_option(buffer, 'buftype') == 0 and vim.api.nvim_buf_get_option(buffer, 'buflisted')
+end
+
+function M.clear_abnormal()
+    for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_valid(buffer) and not M.is_normal_buffer(buffer) then
+            vim.api.nvim_buf_delete(buffer, { force = true })
+        end
+    end
+end
+
+function M.clear_normal()
+    local cur_buf = vim.api.nvim_get_current_buf()
+    for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_valid(buffer) and M.is_normal_buffer(buffer) and buffer ~= cur_buf then
+            vim.api.nvim_buf_delete(buffer, { force = true })
+        end
+    end
+end
+
 return M
 
