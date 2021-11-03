3e30a5fb49d25977e6205e3f81e7addbde6d3db4
Constantine Bulany <robpirojk@gmail.com>
2021-11-03 05:52:13 +0300

    Disabled folding

--- lua/ll/lang/treesitter.lua
+++ lua/ll/lang/treesitter.lua
@@ -20,6 +20,6 @@ treesitter_configs.setup {
         additional_vim_regex_highlighting = false,
     }
 }
-vim.api.nvim_command('set foldmethod=expr')
-vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
-vim.api.nvim_command('set nofoldenable')
+-- vim.api.nvim_command('set foldmethod=expr')
+-- vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+-- vim.api.nvim_command('set nofoldenable')
--- lua/ll/plugin/config/telescope.lua
+++ lua/ll/plugin/config/telescope.lua
@@ -3,29 +3,28 @@ if not present then
     return print('telescope not found')
 end
 
-local telescope_actions = require("telescope.actions.set")
-
-local attach_mappings = function(_)
-    telescope_actions.select:enhance({
-        post = function()	vim.cmd(":normal! zx zR") end,
-    })
-    return true
-end
+-- local telescope_actions = require("telescope.actions.set")
+-- local attach_mappings = function(_)
+--     telescope_actions.select:enhance({
+--         post = function() vim.cmd(":normal! zx zR") end,
+--     })
+--     return true
+-- end
 
 -- Telescope setup
 tele.setup{
     pickers = {
         find_files = {
             hidden = true,
-            attach_mappings = attach_mappings,
+            -- attach_mappings = attach_mappings,
         },
         live_grep = {
             hidden = true,
-            attach_mappings = attach_mappings,
+            -- attach_mappings = attach_mappings,
         },
         help_tags = {
             hidden = true,
-            attach_mappings = attach_mappings,
+            -- attach_mappings = attach_mappings,
             mappings = {
                 i = {
                     ['<cr>'] = 'select_tab'
@@ -36,11 +35,11 @@ tele.setup{
             },
         },
         file_browser = {
-            attach_mappings = attach_mappings,
+            -- attach_mappings = attach_mappings,
             hidden = true,
         },
         oldfiles = {
-            attach_mappings = attach_mappings,
+            -- attach_mappings = attach_mappings,
             hidden = true,
         },
     },
