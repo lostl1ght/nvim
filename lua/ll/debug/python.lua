225c34a880be6a5753c47b0f580c347a561a556a
Constantine Bulany <robpirojk@gmail.com>
2021-11-17 23:43:42 +0300

    Dap python upd

--- lua/ll/debug/python.lua
+++ lua/ll/debug/python.lua
@@ -2,5 +2,4 @@ local present, py = pcall(require, 'dap-python')
 if not present then
     return print('dap-python not found')
 end
--- Python debugger
-py.setup('~/.pyenv/versions/3.9.7/envs/debugpy/bin/python')
+py.setup('~/.pyenv/versions/debugpy/bin/python')
