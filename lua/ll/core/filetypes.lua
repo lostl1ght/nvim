b42b721db0278aadea5f910a7fcacf99e5db3a50
Constantine Bulany <robpirojk@gmail.com>
2021-12-14 22:03:46 +0300

    file type zsh -> bash for treesitter

--- lua/ll/core/filetypes.lua
+++ lua/ll/core/filetypes.lua
@@ -2,3 +2,4 @@ vim.g.tex_flavor = 'latex'
 vim.cmd('au BufRead,BufNewFile *.ASM set ft=masm')
 vim.cmd('au BufRead,BufNewFile *.asm set ft=masm')
 vim.cmd('au BufRead,BufNewFile .clang-format set ft=yaml')
+vim.cmd('au BufRead,BufNewFile .zsh* set ft=bash')
