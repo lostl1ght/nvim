f1982529314b267f0143acb451a6573a3b04ed81
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:58:26 +0300

    feat: add cmp

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,53 @@
+return {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  config = function()
+    local apairs = require('nvim-autopairs')
+    local Rule = require('nvim-autopairs.rule')
+    apairs.setup({})
+
+    local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+    apairs.add_rules({
+      Rule(' ', ' '):with_pair(function(opts)
+        local pair = opts.line:sub(opts.col - 1, opts.col)
+        return vim.tbl_contains({
+          brackets[1][1] .. brackets[1][2],
+          brackets[2][1] .. brackets[2][2],
+          brackets[3][1] .. brackets[3][2],
+        }, pair)
+      end),
+    })
+    for _, bracket in pairs(brackets) do
+      apairs.add_rules({
+        Rule(bracket[1] .. ' ', ' ' .. bracket[2])
+          :with_pair(function()
+            return false
+          end)
+          :with_move(function(opts)
+            return opts.prev_char:match('.%' .. bracket[2]) ~= nil
+          end)
+          :use_key(bracket[2]),
+      })
+    end
+
+    local punctuation = { ',', ';' }
+    for _, punct in pairs(punctuation) do
+      apairs.add_rules({
+        Rule('', punct)
+          :with_move(function(opts)
+            return opts.char == punct
+          end)
+          :with_pair(function()
+            return false
+          end)
+          :with_del(function()
+            return false
+          end)
+          :with_cr(function()
+            return false
+          end)
+          :use_key(punct),
+      })
+    end
+  end
+}
---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,174 @@
+return {
+  'hrsh7th/nvim-cmp',
+
+  dependencies = {
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'lukas-reineke/cmp-under-comparator' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'saadparwaiz1/cmp_luasnip' },
+  },
+
+  event = { 'InsertEnter', 'CmdLineEnter' },
+
+  opts = {
+    symbol_map = {
+      Text = '',
+      Method = '󰆧',
+      Function = '󰊕',
+      Constructor = '',
+      Field = '󰇽',
+      Variable = '󰂡',
+      Class = '󰠱',
+      Interface = '',
+      Module = '',
+      Property = '󰜢',
+      Unit = '',
+      Value = '󰎠',
+      Enum = '',
+      Keyword = '󰌋',
+      Snippet = '',
+      Color = '󰏘',
+      File = '󰈙',
+      Reference = '',
+      Folder = '󰉋',
+      EnumMember = '',
+      Constant = '󰏿',
+      Struct = '',
+      Event = '',
+      Operator = '󰆕',
+      TypeParameter = '󰅲',
+      VimCommand = '',
+    },
+    window_border = {
+      border = 'none',
+      winhighlight = 'Normal:NormalFloat,CursorLine:Visual,Search:None',
+    },
+  },
+
+  config = function(_, opts)
+    local cmp = require('cmp')
+    local compare = cmp.config.compare
+
+    cmp.setup({
+      -- preselect = cmp.PreselectMode.None,
+      window = {
+        completion = cmp.config.window.bordered(opts.window_border),
+        documentation = cmp.config.window.bordered(opts.window_border),
+        col_offset = -3,
+        side_padding = 0,
+      },
+      mapping = {
+        ['<c-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<c-d>'] = cmp.mapping.scroll_docs(4),
+        ['<c-space>'] = cmp.mapping.complete({ reason = cmp.ContextReason.Manual }),
+        ['<c-e>'] = cmp.mapping.close(),
+        ['<cr>'] = cmp.mapping.confirm({ select = true }),
+        ['<c-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<c-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        --[[
+      ['<tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<s-tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ]]
+      },
+      snippet = {
+        --[[
+      expand = function(args)
+        require('snippy').expand_snippet(args.body)
+      end,
+      ]]
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      sources = {
+        { name = 'luasnip' },
+        -- { name = 'snippy' },
+        { name = 'nvim_lsp' },
+        { name = 'path' },
+        {
+          name = 'buffer',
+          keyword_length = 4,
+          option = {
+            keyword_pattern = [[\k\+]],
+          },
+          get_bufnrs = function()
+            local bufs = {}
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              table.insert(bufs, vim.api.nvim_win_get_buf(win))
+            end
+            return bufs
+          end,
+        },
+      },
+      sorting = {
+        comparators = {
+          compare.offset,
+          compare.exact,
+          compare.score,
+          require('cmp-under-comparator').under,
+          -- compare.locality,
+          compare.kind,
+          compare.sort_text,
+          compare.length,
+          compare.order,
+        },
+      },
+      formatting = {
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, vim_item)
+          local symbol_map = opts.symbol_map
+          if entry.source.name == 'cmdline' then
+            vim_item.kind = ' ' .. symbol_map.VimCommand .. ' '
+            return vim_item
+          end
+          local kind_name = vim_item.kind
+          vim_item.kind = ' ' .. symbol_map[kind_name] .. ' '
+          vim_item.menu = ' (' .. kind_name .. ')'
+          return vim_item
+        end,
+      },
+      experimental = {
+        native_menu = false,
+        ghost_text = true,
+      },
+    })
+
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' },
+      },
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' },
+      }, {
+        { name = 'cmdline' },
+      }),
+    })
+
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
+}
