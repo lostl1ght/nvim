8d4ce07edb9f73db185bc568ea85e308b9c31b0e
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:59:56 +0300

    feat: add mini.bracketed

---
+++ lua/plugins/mini_bracketed.lua
@@ -0,0 +1,13 @@
+return {
+  'echasnovski/mini.bracketed',
+  event = 'VeryLazy',
+  config = function()
+    require('mini.bracketed').setup({
+      comment = { suffix = '' },
+      file = { suffix = '' },
+      treesitter = { suffix = '' },
+      indent = { suffix = '' },
+      oldfile = { suffix = '' },
+    })
+  end,
+}
