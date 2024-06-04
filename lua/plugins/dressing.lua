d77e4a6148e2d0515bdf1ae76a4ef9af445f80e7
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:58:31 +0300

    feat: add dressing

---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,24 @@
+return {
+  'stevearc/dressing.nvim',
+
+  event = 'VeryLazy',
+
+  config = function()
+    require('dressing').setup({
+      input = {
+        relative = 'editor',
+        border = 'single',
+      },
+      select = {
+        nui = {
+          border = {
+            style = 'single',
+          },
+        },
+        builtin = {
+          border = 'single',
+        },
+      },
+    })
+  end,
+}
