a7ddd0514fef11703f5eb54d95a3ce22ba72f956
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 01:00:41 +0300

    feat: add mini.surround

---
+++ lua/plugins/mini_surround.lua
@@ -0,0 +1,27 @@
+return {
+  'echasnovski/mini.surround',
+  event = 'VeryLazy',
+  config = function()
+    require('mini.surround').setup({
+      mappings = {
+        add = 'ma',
+        delete = 'md',
+        find = 'mf',
+        find_left = 'mF',
+        highlight = 'mh',
+        replace = 'mr',
+        update_n_lines = 'mn',
+
+        suffix_last = 'l',
+        suffix_next = 'n',
+      },
+    })
+    vim.keymap.set('n', 'gm', 'm', { desc = 'Marks' })
+    local ok, wk = pcall(require, 'which-key')
+    if ok then
+      wk.register({
+        ['m'] = { name = 'surround' },
+      }, {})
+    end
+  end,
+}
