fc15dd24aa1065a8caa4cb2c7e5a477ff23cea6d
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:56:09 +0300

    feat: add mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+  'williamboman/mason.nvim',
+  cmd = 'Mason',
+  build = ':MasonUpdate',
+  keys = {
+    {
+      '<leader>pm',
+      function()
+        vim.cmd({ cmd = 'Mason' })
+      end,
+      desc = 'Mason',
+    },
+  },
+  config = function()
+    require('mason').setup({
+      ui = {
+        border = 'single',
+        width = 0.8,
+        height = 0.8,
+      },
+    })
+  end,
+}
