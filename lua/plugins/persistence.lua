21186b4642365ce239fcc0c71842ac2b4cd764ff
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:57:23 +0300

    feat: add persistence

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,40 @@
+return {
+  'folke/persistence.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  keys = {
+    {
+      '<leader>qs',
+      function()
+        require('persistence').load()
+      end,
+      desc = 'Load current directory',
+    },
+    {
+      '<leader>ql',
+      function()
+        require('persistence').load({ last = true })
+      end,
+      desc = 'Load last session',
+    },
+    {
+      '<leader>qd',
+      function()
+        require('persistence').stop()
+        vim.notify('stopped', vim.log.levels.INFO, { title = 'Persistence' })
+      end,
+      desc = 'Stop recording session',
+    },
+    {
+      '<leader>qD',
+      function()
+        require('persistence').start()
+        vim.notify('started', vim.log.levels.INFO, { title = 'Persistence' })
+      end,
+      desc = 'Start recording session',
+    },
+  },
+
+  config = function()
+    require('persistence').setup()
+  end,
+}
