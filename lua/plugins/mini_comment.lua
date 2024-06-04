d487fbc834f5922aa6d13f38212744d9d43bd553
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 01:00:15 +0300

    feat: add mini.comment

---
+++ lua/plugins/mini_comment.lua
@@ -0,0 +1,26 @@
+return {
+  'echasnovski/mini.comment',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    init = function()
+      vim.g.skip_ts_context_commentstring_module = true
+    end,
+    config = function()
+      require('ts_context_commentstring').setup({
+        enable_autocmd = false,
+      })
+    end,
+  },
+  config = function()
+    require('mini.comment').setup({
+      options = {
+        ignore_blank_line = true,
+        custom_commentstring = function()
+          return require('ts_context_commentstring').calculate_commentstring()
+            or vim.bo.commentstring
+        end,
+      },
+    })
+  end,
+}
