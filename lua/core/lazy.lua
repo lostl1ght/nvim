31c73a8cb1c45d7411d864e9d8cd7c978867db8b
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:50:48 +0300

    feat: add lazy

---
+++ lua/core/lazy.lua
@@ -0,0 +1,46 @@
+local lazy_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazy_path) then
+  vim.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      '--single-branch',
+      'https://github.com/folke/lazy.nvim.git',
+      lazy_path
+  }, {})
+end
+
+vim.opt.runtimepath:prepend(lazy_path)
+
+require('lazy').setup('plugins', {
+  defaults = {
+    lazy = true,
+  },
+  lockfile = vim.fn.stdpath('state') .. '/lazy-lock.json',
+  dev = {
+    path = vim.fs.normalize('~/dev/plugins'),
+  },
+  ui = {
+    border = 'single',
+  },
+  checker = {
+    enabled = false,
+    notify = false,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
+
+vim.keymap.set('n', '<leader>pl', '<cmd>Lazy<cr>', { desc = 'Lazy' })
