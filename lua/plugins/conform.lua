521e9ece4d527d01916487fc4cc35167fd8e959b
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:55:45 +0300

    feat: add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,45 @@
+return {
+  'stevearc/conform.nvim',
+
+  keys = {
+    {
+      'gf',
+      function()
+        require('conform').format({
+          async = true,
+          lsp_fallback = true,
+        }, function()
+          vim.cmd({ cmd = 'update', mods = { silent = true } })
+        end)
+      end,
+      desc = 'Format',
+      mode = { 'n', 'v' },
+    },
+  },
+
+  config = function()
+    require('conform.formatters.latexindent').args = {
+      '-g',
+      '/dev/null',
+      '-m',
+      '-l',
+      vim.fs.normalize(vim.fn.stdpath('config') .. '/latexindent/indentconfig.yaml'),
+      '-',
+    }
+    require('conform.formatters.rustfmt').args = {
+      '--emit=stdout',
+      '--edition=2021',
+    }
+    require('conform').setup({
+      formatters_by_ft = {
+        bib = { 'latexindent' },
+        c = { 'clang_format' },
+        cpp = { 'clang_format' },
+        go = { 'gofmt' },
+        lua = { 'stylua' },
+        rust = { 'rustfmt' },
+        tex = { 'latexindent' },
+      },
+    })
+  end,
+}
