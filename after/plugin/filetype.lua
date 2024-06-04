cbc24ce7b347b14f399dedf11b7b56547af365f5
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:51:23 +0300

    feat: add filetype detection

---
+++ after/plugin/filetype.lua
@@ -0,0 +1,19 @@
+vim.filetype.add({
+  extension = {
+    h = 'cpp',
+    just = 'just',
+    Justfile = 'just',
+    justfile = 'just',
+    tex = 'tex',
+  },
+  filename = {
+    ['.clang-format'] = 'yaml',
+    ['.clangd'] = 'yaml',
+    ['.zimrc'] = 'zsh',
+    Justfile = 'just',
+    justfile = 'just',
+  },
+  pattern = {
+    ['.*/tmux/.*%.conf'] = 'tmux',
+  },
+})
