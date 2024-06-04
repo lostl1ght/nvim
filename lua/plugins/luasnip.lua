7d0bffdecbe080a415c3979da22ae88e24b4bc69
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-05 00:56:05 +0300

    feat: add luasnip

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,29 @@
+return {
+  'L3MON4D3/LuaSnip',
+  build = 'make install_jsregexp',
+  keys = {
+    {
+      '<c-j>',
+      function()
+        if require('luasnip').jumpable() then
+          require('luasnip').jump(1)
+        end
+      end,
+      mode = { 'i', 's' },
+      desc = 'Snippet: jump forward',
+    },
+    {
+      '<c-k>',
+      function()
+        if require('luasnip').jumpable() then
+          require('luasnip').jump(-1)
+        end
+      end,
+      mode = { 'i', 's' },
+      desc = 'Snippet: jump backward',
+    },
+  },
+  config = function()
+    require('luasnip.loaders.from_vscode').lazy_load()
+  end,
+}
