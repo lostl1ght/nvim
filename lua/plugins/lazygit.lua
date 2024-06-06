bf601df6752800569d00821f7d4eafefb20396be
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-07 00:44:28 +0300

    fix: lazygit plugin spec

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,23 +1,20 @@
-local M = { 'lostl1ght/lazygit.nvim' }
-
-M.cmd = 'Lg'
-
-M.keys = {
-  {
-    '<leader>g',
-    function()
-      require('lazygit').open()
+return {
+  'lostl1ght/lazygit.nvim',
+  cmd = 'Lg',
+  keys = {
+    {
+      '<leader>g',
+      function()
+        require('lazygit').open()
+      end,
+      desc = 'Lazygit',
+    },
+  },
+  dependencies = {
+    'samjwill/nvim-unception',
+    lazy = false,
+    config = function()
+      vim.env['GIT_EDITOR'] = "nvim --cmd 'let g:unception_block_while_host_edits=1'"
     end,
-    desc = 'Lazygit',
   },
 }
-
-M.dependencies = {
-  'samjwill/nvim-unception',
-  lazy = false,
-  config = function()
-    vim.env['GIT_EDITOR'] = "nvim --cmd 'let g:unception_block_while_host_edits=1'"
-  end,
-}
-
-return M
