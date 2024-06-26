54aa2c50667348b82c707e899e54f5a900c69bee
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-26 21:35:40 +0300

    feat: add better colorizer command

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,18 +1,39 @@
 return {
   'NvChad/nvim-colorizer.lua',
-
-  cmd = 'ColorizerToggle',
-
-  keys = {
-    {
-      '<leader>tr',
-      function()
-        vim.cmd({ cmd = 'ColorizerToggle' })
+  init = function()
+    vim.g.loaded_colorizer = true
+    local cmd = 'Colorizer'
+    local commands = {
+      toggle = function()
+        local c = require('colorizer')
+        if c.is_buffer_attached(0) then
+          c.detach_from_buffer(0)
+        else
+          c.attach_to_buffer(0)
+        end
       end,
+      reload = function()
+        require('colorizer').reload_all_buffers()
+      end,
+      attach = function()
+        require('colorizer').attach_to_buffer(0)
+      end,
+      detach = function()
+        require('colorizer').detach_from_buffer(0)
+      end,
+    }
+    vim.api.nvim_create_user_command(cmd, function(data)
+      local prefix = require('util').parse(cmd, data.args)
+      commands[prefix]()
+    end, {
+      nargs = 1,
       desc = 'Colorizer',
-    },
-  },
-
+      complete = function(_, line)
+        return require('util').complete(line, cmd, commands)
+      end,
+    })
+  end,
+  cmd = 'Colorizer',
   config = function()
     require('colorizer').setup({
       filetypes = { '*' },
