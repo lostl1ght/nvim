ebcce7da0b8a299331e1764a20e4b0251b0183f4
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-27 22:54:56 +0300

    feat: rework LspAttach config

--- after/plugin/lsp/keys.lua
+++ after/plugin/lsp/keys.lua
@@ -1,61 +1,58 @@
 local function callback(data)
-  local keys = {
-    {
-      'gr',
-      vim.lsp.buf.rename,
-      desc = 'Rename',
-      buffer = data.buf,
-    },
-    {
-      '<leader>ci',
-      function()
-        local new_state = not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
-        vim.lsp.inlay_hint.enable(new_state, { bufnr = 0 })
-        local msg = 'inlay hints ' .. (new_state and 'enabled' or 'disabled')
-        vim.notify(msg, vim.log.levels.INFO, { title = 'Lsp' })
-      end,
-      desc = 'Inlay hints',
-      buffer = data.buf,
-    },
-    {
-      '<leader>ct',
-      function()
-        vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-      end,
-      desc = 'Toggle diagnostics',
-      buffer = data.buf,
-    },
-  }
-
   local client = vim.lsp.get_client_by_id(data.data.client_id)
   if not client then
     return
   end
 
   local util = require('util')
-  for _, map in ipairs(keys) do
-    util.keymap_set(map)
-  end
-  local opts = {
+  local set = util.keymap_set
+  local wk = util.set_which_key
+
+  set({
+    'gr',
+    vim.lsp.buf.rename,
+    desc = 'Rename',
+    buffer = data.buf,
+  })
+  set({
+    '<leader>ci',
+    function()
+      local new_state = not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
+      vim.lsp.inlay_hint.enable(new_state, { bufnr = 0 })
+      local msg = 'inlay hints ' .. (new_state and 'enabled' or 'disabled')
+      vim.notify(msg, vim.log.levels.INFO, { title = 'Lsp' })
+    end,
+    desc = 'Inlay hints',
+    buffer = data.buf,
+  })
+  set({
+    '<leader>ct',
+    function()
+      vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+    end,
+    desc = 'Toggle diagnostics',
+    buffer = data.buf,
+  })
+
+  wk({
     key = '<leader>c',
     name = 'code',
     buf = data.buf,
-  }
-  util.set_which_key(opts)
+  })
 
   if client.server_capabilities.codeActionProvider then
-    util.keymap_set({
+    set({
       '<leader>ca',
       vim.lsp.buf.code_action,
       mode = { 'n', 'v' },
       desc = 'Code actions',
       buffer = data.buf,
     })
-    util.set_which_key({
+    wk({
       key = '<leader>c',
       name = 'code',
       buf = data.buf,
-      mode = { 'n', 'v' },
+      mode = 'v',
     })
   end
 end
--- after/plugin/lsp/texlab.lua
+++ after/plugin/lsp/texlab.lua
@@ -4,36 +4,33 @@ local function callback(data)
     return
   end
 
-  local opts = {
-    key = '<leader>m',
-    name = '<localleader>',
-    buf = data.buf,
-  }
+  local util = require('util')
+  local set = util.keymap_set
+  local wk = util.set_which_key
 
-  local texlab = {
-    {
+  if client.name == 'texlab' then
+    set({
       '<localleader>b',
       function()
         vim.cmd({ cmd = 'TexlabBuild' })
       end,
       desc = 'Build',
       buffer = data.buf,
-    },
-    {
+    })
+    set({
       '<localleader>f',
       function()
         vim.cmd({ cmd = 'TexlabForward' })
       end,
       desc = 'Forward',
       buffer = data.buf,
-    },
-  }
-  local util = require('util')
-  if client.name == 'texlab' then
-    for _, map in ipairs(texlab) do
-      util.keymap_set(map)
-    end
-    util.set_which_key(opts)
+    })
+
+    wk({
+      key = '<leader>m',
+      name = '<localleader>',
+      buf = data.buf,
+    })
   end
 end
 
