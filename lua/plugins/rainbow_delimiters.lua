2ddb4738ca772173225f42d34e521d427cfbb979
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-30 19:51:21 +0300

    fix: use global rainbow strategy

--- lua/plugins/rainbow_delimiters.lua
+++ lua/plugins/rainbow_delimiters.lua
@@ -7,8 +7,8 @@ return {
     local rd = require('rainbow-delimiters')
     vim.g.rainbow_delimiters = {
       strategy = {
-        -- [''] = rd.strategy['global'], -- https://github.com/HiPhish/rainbow-delimiters.nvim/issues/2
-        [''] = rd.strategy['local'],
+        [''] = rd.strategy['global'], -- https://github.com/HiPhish/rainbow-delimiters.nvim/issues/2
+        -- [''] = rd.strategy['local'],
       },
       query = {
         [''] = 'rainbow-delimiters',
