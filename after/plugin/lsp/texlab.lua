d2f5cd44a178bcca9ad1f21c868f6e9222e6b78f
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-07-01 16:55:19 +0300

    fix: move check for texlab to toplevel

--- after/plugin/lsp/texlab.lua
+++ after/plugin/lsp/texlab.lua
@@ -1,6 +1,6 @@
 local function callback(data)
   local client = vim.lsp.get_client_by_id(data.data.client_id)
-  if not client then
+  if not client or client.name ~= 'texlab' then
     return
   end
 
@@ -8,30 +8,28 @@ local function callback(data)
   local set = util.keymap_set
   local mc = util.set_mini_clue
 
-  if client.name == 'texlab' then
-    set({
-      '<localleader>b',
-      function()
-        vim.cmd({ cmd = 'TexlabBuild' })
-      end,
-      desc = 'Build',
-      buffer = data.buf,
-    })
-    set({
-      '<localleader>f',
-      function()
-        vim.cmd({ cmd = 'TexlabForward' })
-      end,
-      desc = 'Forward',
-      buffer = data.buf,
-    })
+  set({
+    '<localleader>b',
+    function()
+      vim.cmd({ cmd = 'TexlabBuild' })
+    end,
+    desc = 'Build',
+    buffer = data.buf,
+  })
+  set({
+    '<localleader>f',
+    function()
+      vim.cmd({ cmd = 'TexlabForward' })
+    end,
+    desc = 'Forward',
+    buffer = data.buf,
+  })
 
-    mc({
-      key = '<leader>m',
-      name = '<localleader>',
-      buf = data.buf,
-    })
-  end
+  mc({
+    key = '<leader>m',
+    name = '<localleader>',
+    buf = data.buf,
+  })
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
