cb1c5ec7491fbad3cc30498fb26e2a7086fa443e
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-07-12 13:16:49 +0300

    feat: add min.lua

---
+++ min.lua
@@ -0,0 +1,36 @@
+local root = '/tmp/nvim'
+
+for _, name in ipairs({ 'config', 'data', 'state', 'cache' }) do
+  vim.env[('XDG_%s_HOME'):format(name:upper())] = root .. '/' .. name
+end
+
+local lazy_path = root .. '/plugins/lazy.nvim'
+if not vim.uv.fs_stat(lazy_path) then
+  vim.api.nvim_cmd({
+    cmd = '!',
+    args = {
+      'git',
+      'clone',
+      '--filter=blob:none',
+      '--single-branch',
+      'https://github.com/folke/lazy.nvim.git',
+      lazy_path,
+    },
+  }, {})
+end
+vim.opt.runtimepath:prepend(lazy_path)
+
+local plugins = {
+  { 'folke/lazy.nvim' },
+  { 'rebelot/kanagawa.nvim' },
+  -- plugins
+}
+
+vim.g.mapleader = ' '
+
+require('lazy').setup(plugins, {
+  root = root .. '/plugins',
+})
+
+vim.opt.termguicolors = true
+vim.cmd('colorscheme kanagawa')
