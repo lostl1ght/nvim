be2ac836b95af1a031316a15c93ab1f38acbfdc6
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-07-12 18:34:29 +0300

    refactor: group all ts plugins

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,5 +1,6 @@
 local MiniDeps = require('mini.deps')
-local add, now = MiniDeps.add, MiniDeps.now
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
 now(function()
   add({
     source = 'nvim-treesitter/nvim-treesitter',
@@ -34,6 +35,7 @@ now(function()
     },
   })
 end)
+
 now(function()
   add({ source = 'HiPhish/rainbow-delimiters.nvim' })
   local cmd = 'Rainbow'
@@ -64,4 +66,39 @@ now(function()
       [''] = 250,
     },
   }
+end)
+
+later(function()
+  add({ source = 'm-demare/hlargs.nvim' })
+  require('hlargs').setup({
+    excluded_argnames = {
+      usages = {
+        python = {},
+        lua = {},
+      },
+    },
+  })
+end)
+
+later(function()
+  add({ source = 'nvim-treesitter/nvim-treesitter-context' })
+  vim.keymap.set('n', 'grc', '<cmd>Context toggle<cr>', { desc = 'Context' })
+  local cmd = 'Context'
+  local commands = {
+    toggle = function() require('treesitter-context').toggle() end,
+    disable = function() require('treesitter-context').disable() end,
+    enable = function() require('treesitter-context').enable() end,
+  }
+  vim.api.nvim_create_user_command(cmd, function(data)
+    local prefix = require('util').parse(cmd, data.args)
+    commands[prefix]()
+  end, {
+    nargs = 1,
+    desc = 'Treesitter context',
+    complete = function(_, line) return require('util').complete(line, cmd, commands) end,
+  })
+  require('treesitter-context').setup({ enable = false })
+  vim.api.nvim_del_user_command('TSContextEnable')
+  vim.api.nvim_del_user_command('TSContextDisable')
+  vim.api.nvim_del_user_command('TSContextToggle')
 end)
