2d79454e452de1f6843683456b01dd17a6207a88
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-07-15 20:17:28 +0300

    refactor: minimize min.lua

--- min.lua
+++ min.lua
@@ -7,15 +7,6 @@ end
 local path_package = vim.fn.stdpath('data') .. '/site/'
 local deps_path = path_package .. 'pack/deps/start/mini.deps'
 if not vim.uv.fs_stat(deps_path) then
-  if vim.fn.executable('git') == 0 then
-    vim.api.nvim_echo({
-      { 'git is not installed\n', 'ErrorMsg' },
-      { 'Press any key to exit...', 'MoreMsg' },
-    }, true, {})
-    vim.fn.getchar()
-    vim.cmd('quit')
-  end
-  vim.api.nvim_echo({ { 'Installing `mini.deps`', 'WarningMsg' } }, true, {})
   -- stylua: ignore
   vim.system({
     'git',   'clone',   '--filter=blob:none',  '--single-branch',
@@ -23,19 +14,9 @@ if not vim.uv.fs_stat(deps_path) then
   }):wait()
   vim.cmd('packadd mini.deps')
   vim.cmd('helptags ALL')
-  vim.api.nvim_echo({ { 'Installed `mini.deps`', 'MoreMsg' } }, true, {})
 end
 
-local ok, minideps = pcall(require, 'mini.deps')
-
-if not ok then
-  vim.api.nvim_echo({
-    { ('Unable to load `mini.deps` from: %s\n'):format(deps_path), 'ErrorMsg' },
-    { 'Press any key to exit...', 'MoreMsg' },
-  }, true, {})
-  vim.fn.getchar()
-  vim.cmd('quit')
-end
+local minideps = require('mini.deps')
 
 minideps.setup({ path = { package = path_package } })
 
