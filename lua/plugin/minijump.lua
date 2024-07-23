66139d0358ca3cf7a8537b05235d827e523895e5
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-07-23 11:29:38 +0300

    fix: change jump2d mapping

--- lua/plugin/minijump.lua
+++ lua/plugin/minijump.lua
@@ -4,9 +4,8 @@ local add, later = minideps.add, minideps.later
 later(function()
   add({ source = 'echasnovski/mini.jump2d' })
   require('mini.jump2d').setup({
-    view = {
-      dim = true,
-    },
+    view = { dim = true },
+    mappings = { start_jumping = 'm' },
   })
 end)
 
