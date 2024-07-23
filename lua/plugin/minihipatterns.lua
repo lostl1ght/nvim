7005a39854d57a9df9b7589f59cac264b4d147f4
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-07-23 19:27:09 +0300

    fix: remove comment patterns

--- lua/plugin/minihipatterns.lua
+++ lua/plugin/minihipatterns.lua
@@ -23,11 +23,6 @@ later(function()
   local minihipatterns = require('mini.hipatterns')
   minihipatterns.setup({
     highlighters = {
-      fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-      hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-      todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-      note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
-
       hex_color = minihipatterns.gen_highlighter.hex_color({
         style = 'inline',
         inline_text = '⬤ ',
