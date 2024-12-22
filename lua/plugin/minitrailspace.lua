108d8b27c313d86d231c49ccfe3742cad1fd02dd
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-12-22 21:08:36 +0300

    fix: do not disable trailspace highlight

--- lua/plugin/minitrailspace.lua
+++ lua/plugin/minitrailspace.lua
@@ -2,12 +2,12 @@ local minideps = require('mini.deps')
 local add, later = minideps.add, minideps.later
 
 later(function()
+  local option_name = 'autorim_disable'
   add({ source = 'echasnovski/mini.trailspace' })
   require('mini.trailspace').setup({})
   local function toggle(buf)
-    vim.b[buf].minitrailspace_disable = not vim.b[buf].minitrailspace_disable
-    require('mini.trailspace').highlight()
-    local state = vim.b.minitrailspace_disable and 'disabled' or 'enabled'
+    vim.b[buf][option_name] = not vim.b[buf][option_name]
+    local state = vim.b[buf][option_name] and 'disabled' or 'enabled'
     vim.notify(state, vim.log.levels.INFO, { title = 'Trailspace' })
   end
   local cmd = 'Trim'
@@ -25,7 +25,7 @@ later(function()
   vim.api.nvim_create_autocmd('BufWritePre', {
     callback = function(data)
       local buf = data.buf
-      local enabled = not vim.b[buf].minitrailspace_disable
+      local enabled = not vim.b[buf][option_name]
       if enabled then
         local minitrailspace = require('mini.trailspace')
         minitrailspace.trim()
@@ -35,4 +35,5 @@ later(function()
     group = vim.api.nvim_create_augroup('TrimWhiteSpace', {}),
     desc = 'Trim trailing whitespace',
   })
+  vim.keymap.set('n', '\\t', function() toggle(0) end, { desc = 'Trim whitespace' })
 end)
