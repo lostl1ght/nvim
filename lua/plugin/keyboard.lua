66169ce50c53d9130a6a82e0317370d476df89e3
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2025-03-02 22:40:37 +0300

    feat: add keymap switch

--- lua/plugin/keyboard.lua
+++ lua/plugin/keyboard.lua
@@ -2,6 +2,17 @@ local minideps = require('mini.deps')
 local add, later = minideps.add, minideps.later
 
 later(function()
+  add({ source = 'lostl1ght/keymap-switch.nvim' })
+  require('keymap_switch').setup({ keymap = 'russian-jcukenwin' })
+  vim.keymap.set(
+    { 'i', 'c', 'n', 'x' },
+    '<c-\\>',
+    '<plug>(keymap-switch)',
+    { desc = 'Switch layout' }
+  )
+end)
+
+later(function()
   add({ source = 'kawre/neotab.nvim' })
   require('neotab').setup({
     pairs = {
