97d003dd4e2d85b9084cafb98241d441ed3b7202
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2025-03-02 22:40:28 +0300

    refactor: move neotab

--- lua/plugin/neotab.lua
+++
@@ -1,7 +0,0 @@
-local minideps = require('mini.deps')
-local add, later = minideps.add, minideps.later
-
-later(function()
-  add({ source = 'kawre/neotab.nvim' })
-  require('neotab').setup({ act_as_tab = false })
-end)
